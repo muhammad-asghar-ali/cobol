2a165fb028b5930f86f57f131915abadea8e91dc
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-01-01 17:05:56 +0500

    feat(): data classification

---
+++ cobol03.cob
@@ -0,0 +1,27 @@
+       >>SOURCE FORMAT FREE
+       *> data classification
+IDENTIFICATION DIVISION. 
+PROGRAM-ID. cobol03.
+AUTHOR. asgr.
+DATE-WrITTEN. Dec 29 2023
+ENVIRONMENT DIVISION.
+CONFIGURATION SECTION.
+SPECIAL-NAMES.
+       CLASS PassingScore IS "A" THRU "C", "D".
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+01 Age PIC 99 VALUE 0.
+01 Grade PIC 99 VALUE 0.
+01 Score PIC X(1) VALUE "B".
+01 CanVoteFlag PIC 9 VALUE 0.
+       88 CanVote VALUE 1.
+       88 CantVote VALUE 0.
+01 TestNumber PIC X.
+       88 IsPrime VALUE "1", "3", "5", "7".
+       88 IsOdd VALUE "1", "3", "5", "7", "9".
+       88 IsEven VALUE "2", "4", "6", "8".
+
+PROCEDURE DIVISION.
+
+STOP RUN.
